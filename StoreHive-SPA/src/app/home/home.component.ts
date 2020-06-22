ee6654e1c8675bded3fbb0b62a8686e21e874248
cidospark <cidospark27@gmail.com>
2020-06-22 12:58:52 +0100

    Get products method added to market place controller

--- StoreHive-SPA/src/app/home/home.component.ts
+++ StoreHive-SPA/src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   }
 
   loadStores() {
-    this.http.get('http://localhost:5000/api/marketplace/getStores').subscribe(response => {
+    this.http.get('http://localhost:5000/api/marketplace/getProducts').subscribe(response => {
       this.stores = response;
     }, error => {
       console.log(error);
